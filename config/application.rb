de2fa84ce24ff37cf3ef8f860e6fcca5c7c796e1
Steven Watkins <steven.watkins78@gmail.com>
2016-04-03 22:01:38 -0400

    ApiConstraints tests passing

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,15 @@ module MarketPlaceApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_gramework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
   end
 end
