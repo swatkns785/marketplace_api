b57b81b2c1030b0d407c4f7f6fe15904c00438f9
Steven Watkins <steven.watkins78@gmail.com>
2016-04-04 11:23:05 -0400

    added upate action on users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -53,4 +53,39 @@ describe Api::V1::UsersController do
       it { should respond_with 422 }
     end
   end
+
+  describe "PUT/PATCH #update" do
+
+    context "when is successfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+                         user: { email: "newemail@comoestas.com" } },
+                         format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newemail@comoestas.com"
+      end
+
+      it { should respond_with 200 }
+    end
+  end
+
+  context "when is not created" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      patch :update, { id: @user.id,
+                       user: { email: "bademail.com"} },
+                       format: :json
+    end
+
+    it "renders an errors json" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:errors][:email]).to include "is invalid"
+    end
+
+    it { should respond_with 422 }
+  end
 end
